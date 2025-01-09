79f88c4040bf4c0bca523a3333135926ea74d676
mario.ezequiel@hotmail.com <mario.ezequiel@hotmail.com>
2025-01-09 11:28:40 +0100

    addition of ch4 file to revise if worked

--- project1/main.c
+++ project1/main.c
@@ -4,15 +4,14 @@
 
 //declare function that are part of your codebase
 void read_nuclear_repulsion(trexio_t* file);
-void read_electron_up_num(trexio_t* file);
+void read_electron_up_num(trexio_t* file, int32_t* n_up);
 void read_mo_data(trexio_t* file, int32_t* mo_num, double** mo_energy);
 void read_one_electron_integrals(trexio_t* file, int32_t mo_num, double* core_hamiltonian);
 void read_two_electron_integrals(trexio_t* file, int64_t* n_integrals, int32_t** index, double** value);
 
-
 int main() {
     trexio_exit_code rc;
-    trexio_t* file = trexio_open("c2h2.h5", 'r', TREXIO_AUTO, &rc);
+    trexio_t* file = trexio_open("ch4.h5", 'r', TREXIO_AUTO, &rc);
 
     if (rc != TREXIO_SUCCESS) {
         printf("Error opening file.\n");
@@ -20,13 +19,18 @@ int main() {
     }
 
     read_nuclear_repulsion(file);
-    read_electron_up_num(file);
+    // Declare variable to store the number of spin-up electrons
+    int32_t n_up;
+    // read the number of spin-up electrons 
+    read_electron_up_num(file, &n_up);
 
     int32_t mo_num;
     double* mo_energy;
     read_mo_data(file, &mo_num, &mo_energy);
-    // saved the number of MOs into a separate variable
-    int32_t saved_mo = mo_num;
+// occupied number of orbitals
+   int32_t saved_mo_num = n_up;
+    // Save the number of molecular orbitals into a separate variable
+   // int32_t saved_mo_num = mo_num;
 
     double* core_hamiltonian = malloc(mo_num * mo_num * sizeof(double));
     read_one_electron_integrals(file, mo_num, core_hamiltonian);
@@ -41,6 +45,9 @@ int main() {
 
     free(mo_energy);
     trexio_close(file);
+
+    // Optionally print the saved number of molecular orbitals for verification
+    printf("Saved number of molecular orbitals: %d\n", saved_mo_num);
 
     return 0;
 }
