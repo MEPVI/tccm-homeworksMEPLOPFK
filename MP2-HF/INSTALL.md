9e76b158f58e09c6d2f495820cecd38b9973a684
mario.ezequiel@hotmail.com <mario.ezequiel@hotmail.com>
2025-01-10 21:59:56 +0100

    MP2-HF done, complete

---
+++ MP2-HF/INSTALL.md
@@ -0,0 +1,15 @@
+# Installation Instructions
+
+1. Install a C compiler (e.g., GCC).
+2. Navigate to the `src/` directory.
+3. Compile the program using:
+   ```
+   gcc -o program main.c HF.c MP2.c -lm
+or change your directory to your library in the makefile, then run make.
+   
+```
+4. Run the program using:
+   ```
+   ./program
+   ```
+
---
+++ MP2-HF/src/HF.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+//C function to obtain the HF energy
+double HF(int32_t N_occ, int32_t N_mo, double E_NN, double* const one_e_int, \
+		int64_t nInts, int32_t* index, double* const two_e_int){
+
+	//Initialize one-and two-electron summations to zero
+	double one_e_sum = 0;
+	double two_e_sum = 0;
+
+	//Add iteratively the one-electron terms
+	for (int i = 0; i < N_occ; i++) {
+		//Add new term to the one electron summation
+		one_e_sum += *(one_e_int + (N_mo * i + i)); //Reading diagonal of array
+	}
+
+	//Add iteratively the two-electron terms
+	for (int n = 0; n < nInts; n++) { //Iterate over integral array just once to make memory read process more efficient
+		//Define indeces for the integral "n"
+		int i = index[4*n];
+		int j = index[4*n+1];
+		int k = index[4*n+2];
+		int l = index[4*n+3];
+
+		//Only use if all indeces belong to occupied orbitals
+		if (i < N_occ && j < N_occ && k < N_occ && l < N_occ) {
+			//Case 1: All indeces are equal
+			if (i == j && j == k && k == l){
+				two_e_sum += two_e_int[n];
+			}
+			//Case 2: Columb integral
+			else if (i == k && j == l){
+				two_e_sum += 4*two_e_int[n];
+			}
+			//Case 3: Exchange integral
+			else if (i == j && k == l){
+				two_e_sum -= 2*two_e_int[n];
+			}
+		}
+	}
+
+	//Obtain the HF Energy by summing all the terms
+	double E_HF = E_NN + 2 * one_e_sum + two_e_sum;
+
+	return E_HF;
+}
